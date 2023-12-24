bfd6aa2e0a84f3790a1e25e3f24615f47bec0b64
thi99 <nguyendinhthi23041999@gmail.com>
2023-12-24 20:49:31 +0700

    complete HomeSectionCarosel

--- src/customer/components/HomeSectionCarosel/HomeSectionCarosel.jsx
+++ src/customer/components/HomeSectionCarosel/HomeSectionCarosel.jsx
@@ -1,44 +1,51 @@
 import { Button } from '@mui/material';
-import React, {useState   } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
 import HomeSectionCard from '../HomeSectionCard/HomeSectionCard';
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 const HomeSectionCarosel = (props) => {
-    const [activeIndex,setActiveIndex] = useState(0)
+    const carousel = useRef(AliceCarousel)
+    const [activeIndex, setActiveIndex] = useState(0)
+    useEffect(() => {
+        console.log(items.length)
+    }, carousel);
     const responsive = {
         0: { items: 1 },
-        568: { items: 3 },
-        1024: { items: 5 },
+        720: { items: 3 },
+        1024: { items: 5.5 },
     };
-    const items = props.data.map(item => <HomeSectionCard product={item} />)
-    const slidePrev = () => setActiveIndex(activeIndex - 1)
-    const slideNext = () => setActiveIndex(activeIndex + 1)
-    const syncActiveIndex=({item})=>setActiveIndex(item)
+    const items = props.data.slice(10).map(item => <HomeSectionCard product={item} />)
     return (
         <div className='px-4 lg:px-8 border'>
             <div className='relative p-5'>
                 <AliceCarousel
+                    key="carousel"
+                    mouseTracking
                     items={items}
                     disableButtonsControls
                     disableDotsControls
                     responsive={responsive}
-                    infinite
-                    activeIndex={activeIndex}
-                    renderSlideInfo={syncActiveIndex}
+                    ref={carousel}
                 />
-                <Button variant='contained' className='z-50'
+                    <Button variant='contained' className='z-50' disabled = {activeIndex == items.length - 5?true : false}
+                        sx={{
+                            possition: 'absolute', top: '8rem', right: '0rem', transform: 'translateX(2040%) translateY(-900%) rotate(90deg)'
+                            , bgcolor: 'white' 
+                        }} onClick={(e) => {
+                            setActiveIndex(activeIndex + 1)
+                            carousel?.current?.slideNext(e)
+                        }}>
+                        <KeyboardArrowLeftIcon sx={{ transform: 'rotate(90deg)', color: 'black' }} />
+                    </Button>
+                <Button variant='contained' className='z-50' disabled = {activeIndex == 0?true : false}
                     sx={{
-                        possition: 'absolute', top: '8rem', right: '0rem', transform: 'translateX(2040%) translateY(-850%) rotate(90deg)'
+                        possition: 'absolute', transform: 'translateX(-230%) translateY(-550%) rotate(90deg)'
                         , bgcolor: 'white'
-                    }} onClick={slideNext}>
-                    <KeyboardArrowLeftIcon sx={{ transform: 'rotate(90deg)', color: 'black' }} />
-                </Button>
-                <Button variant='contained' className='z-50'
-                    sx={{
-                        possition: 'absolute', top: '8rem', right: '0rem', transform: 'translateX(-230%) translateY(-850%) rotate(90deg)'
-                        , bgcolor: 'white'
-                    }} onClick={slidePrev}>
+                    }} onClick={(e) => {
+                        setActiveIndex(activeIndex - 1)
+                        carousel?.current?.slidePrev(e)
+                    }}>
                     <KeyboardArrowLeftIcon sx={{ transform: 'rotate(-90deg)', color: 'black' }} />
                 </Button>
             </div>
